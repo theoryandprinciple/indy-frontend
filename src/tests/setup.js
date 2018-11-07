3a15d47f6d5d22f765d2c7e6b1eb5bcd39a5de19
geoff5802 <keating.geoff@gmail.com>
2018-11-07 14:59:58 -0500

    clean up setup file

--- src/tests/setup.js
+++ src/tests/setup.js
@@ -1,10 +1,9 @@
-// setup file
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
+configure({ adapter: new Adapter() });
+
+//  RESOURCES
 // https://redux.js.org/recipes/writingtests
 // https://airbnb.io/enzyme/docs/installation/index.html
 // https://devhints.io/enzyme
-
-configure({ adapter: new Adapter() });
-// end setup
