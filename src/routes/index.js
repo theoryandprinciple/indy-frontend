6dde6fe5506a5644de95f6387bc3d38e0e54cebb
geoff5802 <keating.geoff@gmail.com>
2018-12-06 16:38:19 -0500

    make login accessible

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,19 +2,20 @@ import React from 'react'
 import { Switch, Route, Link } from 'react-router-dom';
 import Home from '../containers/home/containers'
 import About from '../containers/about'
+import Login from '../containers/login/containers'
 
 const App = () => (
     <Route
         render={({ location }) => (
             <React.Fragment>
                 <header>
-                  <Link to="/">Home</Link>
-                  <Link to="/about">About</Link>
+                  <Link to="/">Home</Link> | <Link to="/about">About</Link> | <Link to="/login">Login</Link>
                 </header>
                 <main>
                     <Switch location={location}>
                         <Route exact path="/" component={Home} />
                         <Route exact path="/about" component={About} />
+                        <Route exact path="/login" component={Login} />
                     </Switch>
                 </main>
             </React.Fragment>
