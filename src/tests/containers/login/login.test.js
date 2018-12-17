9975a70ee0fb71d9229484b28d612ed66457511c
Brian Oickle <brian.oickle@gmail.com>
2018-12-17 14:09:23 -0500

    Merge

--- src/tests/containers/login/login.test.js
+++ src/tests/containers/login/login.test.js
@@ -35,3 +35,30 @@ it('test login', async() => {
   });
 
 });
+
+it('test logout', async() => {
+
+  mockAxios.post.mockImplementationOnce(() =>
+   Promise.resolve({ data: testHelper.loginToken, status: 200 })
+  );
+
+  mockAxios.get.mockImplementationOnce(() =>
+    Promise.resolve({ data: { role: testHelper.loginRole }, status: 200 })
+  );
+
+  let expectedActions =
+       [{"payload": [testHelper.loginEmail, testHelper.loginPassword], "type": "LOGIN_ATTEMPT"},
+       {"payload": {"artifacts": null, "credentials": {"role": testHelper.loginRole, "token": testHelper.loginToken}},
+       "type": "LOGIN_SUCCESS"}];
+
+  await store.dispatch(AuthActions.login(testHelper.loginEmail, testHelper.loginPassword)).then(async () => {
+
+    expect(store.getActions()).toEqual(expectedActions);
+    expect(mockAxios.post).toHaveBeenCalledWith('/login', {"email": testHelper.loginEmail, "password": testHelper.loginPassword}, { responseType: 'text' });
+    expect(mockAxios.post).toHaveBeenCalledTimes(1);
+    expect(mockAxios.get).toHaveBeenCalledWith('/user', {"headers": {"authorization": testHelper.loginToken}});
+
+    return expect(mockAxios.get).toHaveBeenCalledTimes(1);
+  });
+
+});
