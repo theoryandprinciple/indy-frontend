6dada8e5db111d465e2935d51a427ea652fb10d9
geoff5802 <keating.geoff@gmail.com>
2019-06-27 12:09:43 -0400

    update packages

--- src/routes/home/components/index.js
+++ src/routes/home/components/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { history } from '../../../store';
 
 const Admin = () => (
     <div>
         <h1>Basic Home Page</h1>
         <p>stuffs</p>
+        <button type="button" onClick={() => history.push('/admin')}>Admin</button>
     </div>
 );
 
