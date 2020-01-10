15d7ec5419f9674d7344e104c5f5acf75e0dc62d
geoff5802 <keating.geoff@gmail.com>
2020-01-10 10:16:38 -0500

    fixed left elements in flow builder

--- src/routes/admin/beaver/flow-builder/index.js
+++ src/routes/admin/beaver/flow-builder/index.js
@@ -21,56 +21,57 @@ const Flow = ({ classes }) => (
             <DndProvider backend={HTML5Backend}>
                 <div className="row">
                     <div className="col-auto">
-                        <div className="row">
-                            <div className="col">
-                                <h5>SECTION BREAK</h5>
+                        <div style={{ position: 'sticky', top: 0 }}>
+                            <div className="row">
+                                <div className="col">
+                                    <h5>SECTION BREAK</h5>
+                                </div>
                             </div>
-                        </div>
-                        <div className="row">
-                            <div className="col-6">
-                                <Section text="New Section" />
+                            <div className="row">
+                                <div className="col-6">
+                                    <Section text="New Section" />
+                                </div>
+                                <div className="col-6" />
                             </div>
-                            <div className="col-6" />
-                        </div>
 
-                        <div className="row">
-                            <div className="col">
-                                <h5>QUESTION FIELDS</h5>
+                            <div className="row">
+                                <div className="col">
+                                    <h5>QUESTION FIELDS</h5>
+                                </div>
                             </div>
-                        </div>
 
-                        <div className="row">
-                            <div className="col-6">
-                                <Question text="Radio Buttons" inputType={QuestionTypes.RADIO} />
-                                <Question text="Checkboxes" inputType={QuestionTypes.CHECKBOX} />
-                                <Question text="Short Text" inputType={QuestionTypes.SHORT_TEXT} />
+                            <div className="row">
+                                <div className="col-6">
+                                    <Question text="Radio Buttons" inputType={QuestionTypes.RADIO} />
+                                    <Question text="Checkboxes" inputType={QuestionTypes.CHECKBOX} />
+                                    <Question text="Short Text" inputType={QuestionTypes.SHORT_TEXT} />
+                                </div>
+                                <div className="col-6">
+                                    <Question text="Long Text" inputType={QuestionTypes.LONG_TEXT} />
+                                    <Question text="Number" inputType={QuestionTypes.NUMBER} />
+                                    {/* REMOVED FOR MVP
+                                    <Question text="Date" inputType={QuestionTypes.DATE} />
+                                    <Question text="File Uploader" inputType={QuestionTypes.FILE_UPLOAD} />
+                                    <Question text="Jurisdiction" inputType={QuestionTypes.JURISDICTION} />
+                                    <Question text="Legal Disclaimer" inputType={QuestionTypes.LEGAL_DISCLAIMER} />
+                                    */}
+                                </div>
                             </div>
-                            <div className="col-6">
-                                <Question text="Long Text" inputType={QuestionTypes.LONG_TEXT} />
-                                <Question text="Number" inputType={QuestionTypes.NUMBER} />
-                                {/* REMOVED FOR MVP
-                                <Question text="Date" inputType={QuestionTypes.DATE} />
-                                <Question text="File Uploader" inputType={QuestionTypes.FILE_UPLOAD} />
-                                <Question text="Jurisdiction" inputType={QuestionTypes.JURISDICTION} />
-                                <Question text="Legal Disclaimer" inputType={QuestionTypes.LEGAL_DISCLAIMER} />
-                                */}
-                            </div>
-                        </div>
 
-                        <div className="row">
-                            <div className="col">
-                                <h5>OUTPUT</h5>
+                            <div className="row">
+                                <div className="col">
+                                    <h5>OUTPUT</h5>
+                                </div>
+                            </div>
+                            <div className="row">
+                                <div className="col-6">
+                                    <Output text="Email Generation" inputType={OutputTypes.EMAIL} />
+                                </div>
+                                <div className="col-6">
+                                    <Output text="Content" inputType={OutputTypes.CONTENT} />
+                                </div>
                             </div>
                         </div>
-                        <div className="row">
-                            <div className="col-6">
-                                <Output text="Email Generation" inputType={OutputTypes.EMAIL} />
-                            </div>
-                            <div className="col-6">
-                                <Output text="Content" inputType={OutputTypes.CONTENT} />
-                            </div>
-                        </div>
-
                     </div>
                     <div className="col">
                         <SectionWrapper />
