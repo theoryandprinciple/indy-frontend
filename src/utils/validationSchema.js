2cb25e0b6a45c001b38049e2f2fcb3f269be12a8
geoff5802 <keating.geoff@gmail.com>
2020-01-28 20:13:55 -0500

    update email validation schema to mimic the backend.

--- src/utils/validationSchema.js
+++ src/utils/validationSchema.js
@@ -1,8 +1,7 @@
 import Joi from '@hapi/joi';
 
 const schema = Joi.object({
-    email: Joi.string()
-        .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }),
+    email: Joi.string().email({ minDomainSegments: 2, tlds: { allow: false } }),
 });
 
 export default schema;
