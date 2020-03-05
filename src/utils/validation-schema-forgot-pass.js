d508623b9f8a30be62f9b37ebf7bec091a5ef598
geoff5802 <keating.geoff@gmail.com>
2020-03-05 13:27:48 -0500

    update login form styles

---
+++ src/utils/validation-schema-forgot-pass.js
@@ -0,0 +1,9 @@
+import Joi from '@hapi/joi';
+
+const schema = Joi.object({
+    email: Joi.string()
+        .email({ minDomainSegments: 2, tlds: { allow: false } })
+        .required(),
+});
+
+export default schema;
--- src/utils/validationSchema.js
+++
@@ -1,7 +0,0 @@
-import Joi from '@hapi/joi';
-
-const schema = Joi.object({
-    email: Joi.string().email({ minDomainSegments: 2, tlds: { allow: false } }),
-});
-
-export default schema;
