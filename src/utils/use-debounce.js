c5f1735f317e717f7a50b0062a265d61ffa5a1bc
geoff5802 <keating.geoff@gmail.com>
2020-05-04 13:00:20 -0400

    update useEffects

--- src/utils/use-debounce.js
+++ src/utils/use-debounce.js
@@ -33,7 +33,7 @@ export default function useDebounce(value, delay, initialBuildComplete) {
         // Only re-call effect if value changes
         // You could also add the "delay" var to inputs array if you ...
         // ... need to be able to change that dynamically.
-        [value],
+        [value, delay, initialBuildComplete],
     );
 
     return debouncedValue;
