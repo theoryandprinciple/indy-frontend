30bd4d8a09c2da4d690f4e8e75ba5c34220fe085
geoff5802 <keating.geoff@gmail.com>
2020-05-08 10:33:27 -0400

    remove beaver and its dependencies

--- src/layouts/admin/index.js
+++ src/layouts/admin/index.js
@@ -1,13 +1,8 @@
 import React from 'react';
 import { Switch, Route, useLocation } from 'react-router-dom';
 import AdminHome from '../../routes/admin';
-import FlowBuilder from '../../routes/admin/beaver/flow-builder';
-import OutputBuilderCreate from '../../routes/admin/beaver/output-builder/components/create';
-import OutputBuilderDashboard from '../../routes/admin/beaver/output-builder/components/dashboard';
 import withRoot from '../../wiring/with-root';
 import AdminHeader from '../../components/admin/header';
-import FlowDataProvider from '../../routes/admin/beaver/wiring/flow-provider';
-import OutputDataProvider from '../../routes/admin/beaver/wiring/output-provider';
 
 import themer from '../../styles/material-theme';
 
@@ -17,24 +12,11 @@ const AdminLayout = () => {
         <React.Fragment>
             <AdminHeader />
             <main>
-                <FlowDataProvider>
-                    <OutputDataProvider>
-                        <Switch location={location}>
-                            <Route exact path="/admin">
-                                <AdminHome />
-                            </Route>
-                            <Route exact path="/admin/flow-builder">
-                                <FlowBuilder />
-                            </Route>
-                            <Route exact path="/admin/output-builder">
-                                <OutputBuilderDashboard />
-                            </Route>
-                            <Route exact path="/admin/output-builder/create">
-                                <OutputBuilderCreate />
-                            </Route>
-                        </Switch>
-                    </OutputDataProvider>
-                </FlowDataProvider>
+                <Switch location={location}>
+                    <Route exact path="/admin">
+                        <AdminHome />
+                    </Route>
+                </Switch>
             </main>
         </React.Fragment>
     );
