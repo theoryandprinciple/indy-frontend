892c5b7ebce031c1132097080be5e5e79fca1ea4
geoff5802 <keating.geoff@gmail.com>
2020-07-31 10:37:20 -0400

    resolve error boundry bug

--- src/components/ErrorFallback/index.js
+++ src/components/ErrorFallback/index.js
@@ -1,19 +1,25 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import Button from '@material-ui/core/Button';
-import { withStyles } from '@material-ui/core/styles';
 import { useHistory } from 'react-router-dom';
 import Typography from '@material-ui/core/Typography';
 
-import Styles from './styles';
+const styles = () => ({
+    wrapper: {
+        width: '100%',
+        height: '100vh',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
 
-const ErrorBoundry = ({ classes }) => {
+const ErrorBoundry = () => {
     const history = useHistory();
     return (
-        <main className={`container-fluid ${classes.wrapper}`}>
+        <main className={`container-fluid ${styles.wrapper}`} role="alert">
             <div className="row align-items-center" style={{ height: '100%' }}>
                 <div className="col">
-                    <Typography variant="h1">Ops, there was an error</Typography>
+                    <Typography variant="h1">Oops, there was an error</Typography>
 
                     <Button onClick={() => history.push('/')}>
                         Reload Site
@@ -24,8 +30,4 @@ const ErrorBoundry = ({ classes }) => {
     );
 };
 
-ErrorBoundry.propTypes = {
-    classes: PropTypes.object.isRequired,
-};
-
-export default withStyles(Styles)(ErrorBoundry);
+export default ErrorBoundry;
