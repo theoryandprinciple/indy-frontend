0b8a3ee9bae76b01bc26680ee912ec8a3b8c9b5a
Erin Doyle <erindoyle8080@gmail.com>
2020-07-31 12:48:52 -0400

    Added in serviceWorker.js which normally comes out of the box from create-react-app

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import 'bootstrap-css-only/css/bootstrap-grid.min.css';
 import AuthDataProvider from './routes/login/wiring/auth-provider';
 import ErrorFallback from './components/ErrorFallback';
 
+import * as serviceWorker from './serviceWorker';
 import ScrollToTop from './utils/scroll-to-top';
 
 import history from './wiring/history';
@@ -34,3 +35,8 @@ ReactDOM.render(
     </Router>,
     document.getElementById('root'),
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
